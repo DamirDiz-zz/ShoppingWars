97028b6e52f20230944c81e43b6d7bd28845e4fa
Damir Dizdarevic <damir.dizdarevic@gmx.at>
2014-12-01 11:03:51 +0100

    collecting items

---
+++ ShoppingWars/Assets/Scripts/Player/PlayerItems.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerItems : MonoBehaviour {
+
+	List<GameObject> collectedItems;
+
+	// Use this for initialization
+	void Start () {
+		collectedItems = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.tag == "Item")
+		{
+			collectedItems.Add(col.gameObject);
+			Destroy(col.gameObject);
+			Debug.Log (collectedItems.Count);
+		}
+	}
+}
