a8005ebb7492d10f672167ec26c988906e4bf2bc
Per Classon <perwclasson@gmail.com>
2014-12-02 14:56:53 +0100

    Fix script

--- ShoppingWars/Assets/Scripts/Player/PlayerItems.cs
+++ ShoppingWars/Assets/Scripts/Player/PlayerItems.cs
@@ -8,7 +8,6 @@ public class PlayerItems : MonoBehaviour {
 	List<GameObject> collectedItems;
 	public string name = "";
 	public Text text;
-	int score = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -26,7 +25,6 @@ public class PlayerItems : MonoBehaviour {
 		if(col.gameObject.tag == "Item")
 		{
 			collectedItems.Add(col.gameObject);
-			score = score + 1;
 			text.text =  name + "\nScore: " + collectedItems.Count;
 			Destroy(col.gameObject);
 			Debug.Log (collectedItems.Count);
