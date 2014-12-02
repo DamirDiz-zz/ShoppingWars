dd2f43aac6cde570e0a6a5f47d25b70540f68b8b
Damir Dizdarevic <damir.dizdarevic@gmx.at>
2014-12-02 14:15:50 +0100

    ItemFactory

--- ShoppingWars/Assets/Scripts/Item/ItemFactory.cs
+++ ShoppingWars/Assets/Scripts/Item/ItemFactory.cs
@@ -4,20 +4,32 @@ using System.Collections.Generic;
 
 public class ItemFactory : MonoBehaviour {
 	
-	public float respawnRate = 3f;        // The time between each shot.
+	public float respawnRate = 1f;
+	public float itemMax = 8f;    
+
+	public GameObject prefab1;
+	public GameObject prefab2;
+	public GameObject prefab3;
+	public GameObject prefab4;
+	public GameObject prefab5;
+	public GameObject prefab6;
+	public GameObject prefab7;
+
+	public Transform spawn1;
+	public Transform spawn2;
+	public Transform spawn3;
+	public Transform spawn4;
+	public Transform spawn5;
+	public Transform spawn6;
+	public Transform spawn7;
 
 	float timer; // A timer to determine when to fire.
 
-
-	/*
-	 * 1) get amount of items
-	 * 2) counter
-	 * 3) wenn anzahl der items unter 9 und timer abgelaufen erzeuge item
-	 */
+	Transform location;
+	GameObject item;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 
 	void Update ()
@@ -28,10 +40,54 @@ public class ItemFactory : MonoBehaviour {
 
 		//get the items with tag
 
-		if(timer >= respawnRate)
+		if(timer >= respawnRate && itemsOnMap.Length < itemMax)
         {
-			timer = 0f;
-			Debug.Log (itemsOnMap.Length);
+			spawn ();
         }
     }
+
+	void spawn () 
+	{
+		timer = 0f;
+
+		int randomPick = Mathf.Abs(Random.Range(1,7));
+
+		if(randomPick == 1){
+			location = spawn1;
+			item = prefab1;
+			Debug.Log("Chose pos 1");
+		}
+		else if(randomPick == 2){
+			location = spawn2;
+			item = prefab2;
+			Debug.Log("Chose pos 2");
+		}
+		else if(randomPick == 3){
+			location = spawn3;
+			item = prefab3;
+			Debug.Log("Chose pos 3");
+		} 
+		else if(randomPick == 4){
+			location = spawn4;
+			item = prefab4;
+			Debug.Log("Chose pos 4");
+		} 
+		else if(randomPick == 5){
+			location = spawn5;
+			item = prefab5;
+			Debug.Log("Chose pos 5");
+		} 
+		else if(randomPick == 6){
+			location = spawn6;
+			item = prefab6;
+			Debug.Log("Chose pos 6");
+		} 
+		else if(randomPick == 7){
+			location = spawn7;
+			item = prefab7;
+			Debug.Log("Chose pos 7");
+		} 
+
+		Instantiate(item, location.transform.position, Quaternion.identity);
+	}
 }
