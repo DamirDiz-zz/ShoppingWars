f4d9537564bedc5875431755cc4dc5ca0917bffc
Per Classon <perwclasson@gmail.com>
2014-12-02 14:55:52 +0100

    Score

--- ShoppingWars/Assets/Scripts/Player/PlayerItems.cs
+++ ShoppingWars/Assets/Scripts/Player/PlayerItems.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 public class PlayerItems : MonoBehaviour {
 
 	List<GameObject> collectedItems;
+	public string name = "";
+	public Text text;
+	int score = 0;
 
 	// Use this for initialization
 	void Start () {
 		collectedItems = new List<GameObject>();
+		text.text = name + "\nScore: 0";
 	}
 	
 	// Update is called once per frame
@@ -21,6 +26,8 @@ public class PlayerItems : MonoBehaviour {
 		if(col.gameObject.tag == "Item")
 		{
 			collectedItems.Add(col.gameObject);
+			score = score + 1;
+			text.text =  name + "\nScore: " + collectedItems.Count;
 			Destroy(col.gameObject);
 			Debug.Log (collectedItems.Count);
 		}
