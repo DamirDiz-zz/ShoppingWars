a5f09e5321e3262051a72ab3bb6b6b72f7473d72
Per Classon <perwclasson@gmail.com>
2014-12-14 16:51:25 +0100

    Particles on “freeze”

--- ShoppingWars/Assets/Scripts/Player/PlayerController.cs
+++ ShoppingWars/Assets/Scripts/Player/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	public string controlsHorizontal;
 	public string controlsVertical;
+	public GameObject particles;
 
 	List<GameObject> collectedItems;
 	Animator anim;
@@ -17,6 +18,8 @@ public class PlayerController : MonoBehaviour {
 	Vector3 movement;
 	Rigidbody playerRigidbody;
 	int floorMask;
+	float timeFreeze;
+	bool isFreezed = false;
 
 
 	void Awake ()
@@ -24,24 +27,27 @@ public class PlayerController : MonoBehaviour {
 		floorMask = LayerMask.GetMask ("Floor");
 		anim = GetComponent <Animator> ();
 		playerRigidbody = GetComponent <Rigidbody> ();
+		particles.SetActive (false);
 		
 	}
 
 	void FixedUpdate ()
 	{
 		if (GameController.isRunning) {
-			// Store the input axes.
-			float h = Input.GetAxisRaw (controlsHorizontal);
-			float v = Input.GetAxisRaw (controlsVertical);
-			
-			Vector3 movement = new Vector3 (h, 0.0f, v) * speed;
-			
-			if (h != 0 || v != 0) {
-				transform.rotation = Quaternion.LookRotation (movement);
+			if (!isFreezed) {
+				// Store the input axes.
+				float h = Input.GetAxisRaw (controlsHorizontal);
+				float v = Input.GetAxisRaw (controlsVertical);
+				
+				Vector3 movement = new Vector3 (h, 0.0f, v) * speed;
+				
+				if (h != 0 || v != 0) {
+					transform.rotation = Quaternion.LookRotation (movement);
+				}
+				
+				anim.SetBool ("IsWalking", (h != 0 || v != 0));
+				playerRigidbody.AddForce (movement * Time.deltaTime * 200, ForceMode.Acceleration);
 			}
-			
-			anim.SetBool ("IsWalking", (h != 0 || v != 0));
-			playerRigidbody.AddForce (movement * Time.deltaTime * 200, ForceMode.Acceleration);
 		}
 	}
 
@@ -52,11 +58,24 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void Update () {
-	
-		if (health <= 0) {
+		if (GameController.isRunning) {
+			if (health <= 0 && !isFreezed) {
+				isFreezed = true;
+				timeFreeze = 5.0f;
+				particles.SetActive (true);
+			}
 
+			if (isFreezed) {
+				timeFreeze -= Mathf.Max(Time.deltaTime, 0f);
+			}
+
+			if (isFreezed && timeFreeze <= 0) {
+				isFreezed = false;
+				health = 100;
+				particles.SetActive (false);
+			}
 		}
-	
+		
 	}
 
 	void OnCollisionEnter (Collision col)
@@ -71,7 +90,7 @@ public class PlayerController : MonoBehaviour {
 		}
 		else if (col.gameObject.tag == "Cart")
 		{
-			health = Mathf.Max(health - 10, 0);
+			health = Mathf.Max(health - Random.Range(30, 60), 0);
 		}
 		text.text = getText();
 	}
